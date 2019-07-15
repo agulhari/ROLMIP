0d3d8b4f8d3fcd3feda184ea870eb35ef303713c
pvogt09 <50047961+pvogt09@users.noreply.github.com>
2019-07-15 14:19:07 +0200

    revert: reverted commenting of gethash in rolmipvar
    Signed-off-by: pvogt09 <50047961+pvogt09@users.noreply.github.com>

--- @rolmipvar/rolmipvar.m
+++ @rolmipvar/rolmipvar.m
@@ -516,13 +516,13 @@ end
 return
 
 
-function index = gethash(exponent,exptable,jump)
-index = 0;
-for contsimplex=1:(length(exponent)) 
-    if (length(exptable{contsimplex}) > 0)
-        [i,j] = find(exptable{contsimplex} - repmat(exponent{contsimplex},size(exptable{contsimplex},1),1) == 0);
-        index = index + (find(histc(i,1:size(exptable{contsimplex},1)) == size(exptable{contsimplex},2)) - 1)*jump(contsimplex);
-    end
-end
-index = index + 1;
-return
+%function index = gethash(exponent,exptable,jump)
+%index = 0;
+%for contsimplex=1:(length(exponent)) 
+%    if (length(exptable{contsimplex}) > 0)
+%        [i,j] = find(exptable{contsimplex} - repmat(exponent{contsimplex},size(exptable{contsimplex},1),1) == 0);
+%        index = index + (find(histc(i,1:size(exptable{contsimplex},1)) == size(exptable{contsimplex},2)) - 1)*jump(contsimplex);
+%    end
+%end
+%index = index + 1;
+%return
